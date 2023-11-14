5e1e79666bc373526ca9e884a200abd0fb465a90
OTMANE <davenamir3@gmail.com>
2023-11-14 16:52:56 +0100

    0

--- main.h
+++ main.h
@@ -1,13 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
 
 
 #endif
-
--- printf.c
+++ printf.c
@@ -1,9 +1,5 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
+#include "main.h"
 
-int _printf(const char *format, ...);
 /**
  * _printf - main func
  * @format: the format string
